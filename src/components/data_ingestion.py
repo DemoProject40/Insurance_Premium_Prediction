3c435c634ab404a743deb5ffb8fb8db5082e750e
Indra Kumar <DemoProject40@gmail.com>
2023-05-05 16:11:42 +0530

    data ingestion

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -22,13 +22,13 @@ class dataIngestion:
         try:
             df=pd.read_csv('notebooks/data/insurance.csv')
             logging.info('Failed to load the pandas dataframe csv file')
-            os.mkdir(os.path.dirname(self.ingestion_config.raw_data_path,exist_ok=True))
+            os.makedirs(os.path.dirname(self.ingestion_config.raw_data_path),exist_ok=True)
             df.to_csv(self.ingestion_config.raw_data_path,index=False)
             logging.info('train_test_split')
             train_set,test_set=train_test_split(df,test_size=0.30)
 
-            train_set.to_csv(self.ingestion_config.train_data_path,Index=False,header=True)
-            test_set.to_csv(self.ingestion_config.test_data_path,Index=False,header=True)
+            train_set.to_csv(self.ingestion_config.train_data_path,index=False,header=True)
+            test_set.to_csv(self.ingestion_config.test_data_path,index=False,header=True)
 
             logging.info('Data ingestion is Completed')
             return (
