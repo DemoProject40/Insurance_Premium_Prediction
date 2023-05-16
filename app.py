f2e16d774e498cdd3baffea20e9f7cc15eefb548
Indra Kumar <ProjectDemo40@gmail.com>
2023-05-16 13:59:59 +0530

    'webapp'

--- app.py
+++ app.py
@@ -1,38 +1,41 @@
-from flask import Flask, render_template, request
-from src.pipeline.prediction_pipeline import Custom_Data, Predict_Pileline
+from flask import Flask,request,render_template,jsonify
+from src.pipeline.prediction_pipeline import CustomData,PredictPipeline
 
 
-app = Flask(__name__)
+application=Flask(__name__)
+
+app=application
+
 
 
 @app.route('/')
 def home_page():
-	return render_template('index.html')
+    return render_template('index.html')
 
-@app.route('/predict',methods = ['GET','POST'])
+@app.route('/predict',methods=['GET','POST'])
+
 def predict_datapoint():
-	if request.method == 'GET':
-		return render_template('form.html')
+    if request.method=='GET':
+        return render_template('form.html')
+    
+    else:
+        data=CustomData(
+            age=request.form.get('age'),
+            sex = request.form.get('sex'),
+            bmi = float(request.form.get('bmi')),
+            children = request.form.get('children'),
+            smoker= request.form.get('smoker'),
+            region = request.form.get('region')
+        )
+        final_new_data=data.get_data_as_dataframe()
+        predict_pipeline=PredictPipeline()
+        pred=predict_pipeline.predict(final_new_data)
 
-	else:
-		data = Custom_Data(
-			age = request.form.get('age'),
-			sex = request.form.get('sex'),
-			bmi = float(request.form.get('bmi')),
-			children = request.form.get('children'),
-			smoker = request.form.get('smoker'),
-			region = request.form.get('region')
-		)
-		
-		final_df = data.get_data_as_dataframe()
-		predict_pipeline = Predict_Pileline()
-		pred = predict_pipeline.predict(final_df)
+        results=round(pred[0],2)
 
-		results = round(pred[0],3)
+        return render_template('result.html',final_result=results)
 
-		return render_template('result.html',final_result = results)
 
-		
 
-if __name__ == '__main__':
-	app.run(host='0.0.0.0',debug=True)
+if __name__=="__main__":
+    app.run(host='0.0.0.0',debug=True)
--- src/pipeline/prediction_pipeline.py
+++ src/pipeline/prediction_pipeline.py
@@ -1,67 +1,62 @@
-import numpy as np 
-import pandas as pd
-from src.exception import CustomException
-from src.logger import logging
-
-from src.utils import load_model
-
 import sys
 import os
+from src.exception import CustomException
+from src.logger import logging
+from src.utils import load_model
+import pandas as pd
 
-class Predict_Pileline:   
+
+class PredictPipeline:
     def __init__(self):
         pass
 
     def predict(self,features):
         try:
-            preprecessor_path = os.path.join('artifacts','preprocessor.pkl')
-            model_path = os.path.join('artifacts','model.pkl')
+            preprocessor_path=os.path.join('artifacts','preprocessor.pkl')
+            model_path=os.path.join('artifacts','model.pkl')
 
-            preprocessor = load_model(preprecessor_path)
-            model = load_model(model_path)
-            logging.info('preprocesor',preprocessor)
-            logging.info('preprocesor',model)
+            preprocessor=load_model(preprocessor_path)
+            model=load_model(model_path)
 
-            data_scaled = preprocessor.transform(features)
+            data_scaled=preprocessor.transform(features)
 
-            pred = model.predict(data_scaled)
+            pred=model.predict(data_scaled)
+            return pred
+            
 
         except Exception as e:
-            logging.info('Exception ocur in predicted pipeline')
+            logging.info("Exception occured in prediction")
             raise CustomException(e,sys)
-
-class Custom_Data:
+        
+class CustomData:
     def __init__(self,
-                age : int,
-            	sex : float,
-                bmi : float,
-                children : int,
-                smoker : float,
-                region : float ):
-
-                self.age = age,
-                self.sex = sex,
-                self.bmi = bmi,
-                self.children = children,
-                self.smoker = smoker,
-                self.region = region
+                 age:int,
+                 sex:str,
+                 bmi:float,
+                 children:int,
+                 smoker:str,
+                 region:str):
+        
+        self.age=age
+        self.sex=sex
+        self.bmi=bmi
+        self.children = children
+        self.smoker = smoker
+        self.region = region
 
     def get_data_as_dataframe(self):
         try:
             custom_data_input_dict = {
-                'age' : [self.age],
-                'sex' : [self.sex],
-                'bmi' : [self.bmi],
-                'children' : [self.children],
-                'smoker' : [self.smoker],
-                'region' : [self.region]                   
+                'age':[self.age],
+                'sex':[self.sex],
+                'bmi':[self.bmi],
+                'children':[self.children],
+                'smoker':[self.smoker],
+                'region':[self.region]
             }
             df = pd.DataFrame(custom_data_input_dict)
-            logging.info('data frame gathered')
+            logging.info('Dataframe Gathered')
             return df
         except Exception as e:
-            logging.info('Exception Occur in prediction pipeline')
+            logging.info('Exception Occured in prediction pipeline')
             raise CustomException(e,sys)
-
-            
-
